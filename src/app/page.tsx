bd2a138817fdcb7121ff1290b3fb04853d610435
fullstacknyc <123508794+4ur4-1@users.noreply.github.com>
2023-01-25 21:10:34 -0500

    feat:show hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/Header";
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <main>
+        <Hero />
+      </main>
+    </>
   );
 }
